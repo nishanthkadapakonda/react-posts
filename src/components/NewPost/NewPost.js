b665a7c7abd5a03af33f3c52a8f0414e0f78b7ca
nishanthkadapakonda <nkadapakonda@aligntech.com>
2018-07-05 15:30:26 +0530

    post clickable

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './NewPost.css';
 
 class NewPost extends Component {
@@ -8,7 +8,17 @@ class NewPost extends Component {
         content: '',
         author: 'Max'
     }
-
+    postDataHandler = () =>{
+        let post = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        }
+     axios.post('https://jsonplaceholder.typicode.com/posts',post).
+     then(response => {
+       console.log(response.data);
+     });
+    }
     render () {
         return (
             <div className="NewPost">
@@ -22,7 +32,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick= {this.postDataHandler}>Add Post</button>
             </div>
         );
     }
